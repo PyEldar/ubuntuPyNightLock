50ad83cf852edbf8dafe0fd34bc8246407455aa9
Jiří Pavlík <485558@mail.muni.cz>
2020-07-12 12:26:26 +0200

    Update host to new url, add eperimental fill empty readings method

--- background_manager.py
+++ background_manager.py
@@ -45,10 +45,31 @@ class Manager:
 
     def generate_data(self):
         logging.debug('generating data')
-
         bgs = [(int(item['sgv']) / 18) for item in self.entries if item.get('sgv') and item.get('date')]
-        datetimes = [datetime.datetime.fromtimestamp(item['date'] / 1000).strftime('%H:%M') for item in self.entries if item.get('sgv') and item.get('date')]
-        self.create_and_save_figure(datetimes, bgs)
+        datetimes = [datetime.datetime.fromtimestamp(item['date'] / 1000) for item in self.entries if item.get('sgv') and item.get('date')]
+        # self._fill_missing_readings(datetimes, bgs)
+        times = [date.strftime("%H:%M") for date in datetimes]
+        self.create_and_save_figure(times, bgs)
+
+    def _fill_missing_readings(self, datetimes, bgs):
+        max_no_reading_time = datetime.timedelta(seconds=350)
+        diffs_between_readings = {i: t - s for i, (s, t) in enumerate(zip(datetimes, datetimes[1:])) if (t - s) > max_no_reading_time}
+        additional_adjustments = 0
+        for i, (index, time_diff) in enumerate(diffs_between_readings.items()):
+            final_index = index + i + additional_adjustments
+            if time_diff > max_no_reading_time * 2:
+                for x in range(1, 4):
+                    datetimes.insert(index + i, datetimes[final_index] + datetime.timedelta(seconds=300 * x))
+                    bgs.insert(final_index, 0)
+                    additional_adjustments += 2
+            elif time_diff > max_no_reading_time * 2:
+                for x in range(1, 3):
+                    datetimes.insert(index + i, datetimes[final_index] + datetime.timedelta(seconds=300 * x))
+                    bgs.insert(final_index, 0)
+                    additional_adjustments += 1
+            else:
+                datetimes.insert(final_index, datetimes[final_index] + datetime.timedelta(seconds=300))
+                bgs.insert(final_index, 0)
 
     def create_and_save_figure(self, datetimes, bgs):
         fig = plt.figure(figsize=(self.resolution[0] / self.dpi, self.resolution[1] / self.dpi), dpi=self.dpi)
